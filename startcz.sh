d625906be1b366b8205a7d590142d36d11444081
Chris Collier <c.collier@bullhorn.com>
2013-10-10 09:46:05 -0400

    initial commit

---
+++ startcz.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+java -javaagent:/home/ccollier/devel/tools/jrebel/jrebel.jar -cp "/usr/share/java/scala-library.jar:target/efind.jar:/usr/share/java/nailgun-0.9.0.jar:/usr/share/java/scalap.jar:/usr/share/java/scala-jline.jar:test.jar:/usr/share/java/scala-compiler.jar:test.jar:/usr/share/java/scala-reflect.jar" com.martiansoftware.nailgun.NGServer
